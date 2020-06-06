2041b3dc2e5ac64899e98d6919ce66fe61bc60fc
Bieg-Moritz <64801200+Bieg-Moritz@users.noreply.github.com>
2020-06-06 09:54:43 +0200

    calculation 2 + 3

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+#include "sub.h"
 int main()
 {
     prinft("tis is my calculator\n");
     
+    double x = 2;
+    double y = 3;
+    printf("%lf - %lf = %lf\n",x,y,mySubFcn(x,y));
 }

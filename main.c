6a554c6776dcfd93d50cdb90e5d6e721811b6560
Bieg-Moritz <64801200+Bieg-Moritz@users.noreply.github.com>
2020-06-06 09:44:10 +0200

    add

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "add.h"
+
+int main()
+{
+    prinft("tis is my calculator\n");
+
+    double x =1;
+    double y =2;
+    printf("%lf + %lf = %lf",x,x,myAddFcn(x,y));
+}
